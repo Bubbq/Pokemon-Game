a64d54195e815515bc254f6cc09a38aa45e0cecf
Bubbq <134325235+Bubbq@users.noreply.github.com>
2023-05-31 13:40:52 -0500

    Update pokemon.cpp
    changed cpp to support new data method "GetTypeAsString"

--- pokemon.cpp
+++ pokemon.cpp
@@ -1,16 +1,16 @@
 #include "pokemon.h"
 
 Pokemon::Pokemon(){
-    
+
     name = "";
     base_attack_name = "";
     heavy_attack_name = "";
-    
+
     evoStage = 0;
     hp = 0;
     base_attack_dmg = 0;
     heavy_attack_dmg = 0;
-    
+
     type = NORMAL;
 }
 
@@ -21,6 +21,28 @@ void Pokemon::Setname(std::string val) { name = val; }
 
 natureType Pokemon::GetType(){return type;}
 void Pokemon::SetType(int val){type = (natureType)val;}
+std::string Pokemon::GetTypeAsString(natureType val){
+
+    switch(val){
+
+        case 1: return "Normal";
+        break;
+
+        case 2: return "Fire";
+        break;
+
+        case 3: return "Water";
+        break;
+
+        case 4: return "Electric";
+        break;
+
+        default: return "Grass";
+        break;
+
+    }
+
+}
 
 int Pokemon::GetevoStage() { return evoStage; }
 void Pokemon::SetevoStage(int val) { evoStage = val; }
@@ -40,13 +62,13 @@ void Pokemon::Setheavy_attack_name(std::string val) { heavy_attack_name = val; }
 int Pokemon::Getheavy_attack_dmg() { return heavy_attack_dmg; }
 void Pokemon::Setheavy_attack_dmg(int val) { heavy_attack_dmg = val; }
 
-void Pokemon::print(){
+void Pokemon::printPokemon(){
     std::cout <<
         "Name: " << name << '\n' <<
-        "Type: " << type << '\n' << 
-        "Evol: " << evoStage << '\n' << 
-        "HP: " << hp << '\n' << 
+        "Type: " << GetTypeAsString(type) << '\n' <<
+       // "Evol: " << evoStage << '\n' <<
+        "HP: " << hp << '\n' <<
         "Base_Attack: " << base_attack_name << ", " << base_attack_dmg << " dmg\n" <<
-        "Heavy_Attack: " << heavy_attack_name << ", " << heavy_attack_dmg << " dmg\n" << 
+        "Heavy_Attack: " << heavy_attack_name << ", " << heavy_attack_dmg << " dmg\n" <<
         '\n';
 }
